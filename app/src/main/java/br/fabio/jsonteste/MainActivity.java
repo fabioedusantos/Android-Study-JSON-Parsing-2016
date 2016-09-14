4698716daa7c4d9cbeb6661ef1e3edc484c6b281
Fábio Eduardo dos Santos <fabioedusantos@gmail.com>
2016-09-13 21:38:00 -0300

    feat(ui): estrutura tela para demonstrar leitura de JSON

--- app/src/main/java/br/fabio/jsonteste/MainActivity.java
+++ app/src/main/java/br/fabio/jsonteste/MainActivity.java
@@ -2,12 +2,31 @@ package br.fabio.jsonteste;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView lblUsuario1;
+    private TextView lblSenha1;
+    private TextView lblUsuario2;
+    private TextView lblSenha2;
+    private TextView lblUsuario3;
+    private TextView lblSenha3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lblUsuario1 = (TextView) findViewById(R.id.lbl_usuario_1);
+        lblSenha1 = (TextView) findViewById(R.id.lbl_senha_1);
+        lblUsuario2 = (TextView) findViewById(R.id.lbl_usuario_2);
+        lblSenha2 = (TextView) findViewById(R.id.lbl_senha_2);
+        lblUsuario3 = (TextView) findViewById(R.id.lbl_usuario_3);
+        lblSenha3 = (TextView) findViewById(R.id.lbl_senha_3);
+    }
+
+    public void ler(View v){
     }
 }
